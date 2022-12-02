702ec7b0e990a0ea3912975cf21152f27f6b1aab
projectnevz@gmail.com <projectnevz@gmail.com>
2022-12-02 15:38:13 +0800

    day2 solution

---
+++ day2/index.js
@@ -0,0 +1,35 @@
+const fs = require("fs");
+
+const map = {
+  A: "Y",
+  B: "Z",
+  C: "X",
+  X: "B",
+  Y: "C",
+  Z: "A",
+};
+
+const input = fs
+  .readFileSync(process.cwd() + "/input.txt", "utf-8")
+  .split("\n")
+  .map((el) => el.split(" "))
+  .slice(0, -1);
+
+const part1 = input.reduce((total, [x, y]) => {
+  total += y === "X" ? 1 : y === "Y" ? 2 : 3;
+  return total + (map[y] === x ? 0 : map[x] === y ? 6 : 3);
+}, 0);
+
+const part2 = input.reduce((total, [x, y]) => {
+  // lol
+  if (y === "X") {
+    total += (x === "A" ? 3 : x === "B" ? 1 : 2) + 0;
+  } else if (y === "Z") {
+    total += (x === "A" ? 2 : x === "B" ? 3 : 1) + 6;
+  } else {
+    total += (x === "A" ? 1 : x === "B" ? 2 : 3) + 3;
+  }
+  return total;
+}, 0);
+
+console.log({ part1, part2 });
