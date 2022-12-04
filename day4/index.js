c894b9ad7afdd3537cd5cf1e5d9cd651ee7fa417
projectnevz@gmail.com <projectnevz@gmail.com>
2022-12-04 14:29:01 +0800

    minor refactor

--- day4/index.js
+++ day4/index.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 
-const counter = condition => {
+const isOverlapping = condition => {
     return condition ? 1 : 0;
 };
 
@@ -10,24 +10,22 @@ const input = fs
     .split("\n");
 
 const pairs = input
-    .map(line => line.split(","))
-    .map(rangePair => rangePair.flatMap(range => range.split("-")).map(Number))
-    .sort((a, b) => a[0] - b[0]);
+    .map(pair => pair.split(","))
+    .map(rangePair => rangePair.flatMap(range => range.split("-")).map(Number));
 
 const p1 = pairs.reduce(
-    (total, rangePair) =>
+    (total, pair) =>
         total +
-        counter(
-            (rangePair[0] >= rangePair[2] && rangePair[1] <= rangePair[3]) ||
-                (rangePair[2] >= rangePair[0] && rangePair[3] <= rangePair[1])
+        isOverlapping(
+            (pair[0] >= pair[2] && pair[1] <= pair[3]) ||
+                (pair[2] >= pair[0] && pair[3] <= pair[1])
         ),
     0
 );
 
 const p2 = pairs.reduce(
-    (total, rangePair) =>
-        total +
-        counter(rangePair[0] <= rangePair[3] && rangePair[1] >= rangePair[2]),
+    (total, pair) =>
+        total + isOverlapping(pair[0] <= pair[3] && pair[1] >= pair[2]),
     0
 );
 
