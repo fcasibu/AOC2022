482c0b4440166c17c048ffdf416919ed5d86352e
projectnevz@gmail.com <projectnevz@gmail.com>
2022-12-04 14:47:36 +0800

    use filter

--- day4/index.js
+++ day4/index.js
@@ -1,9 +1,5 @@
 const fs = require("fs");
 
-const isOverlapping = condition => {
-  return condition ? 1 : 0;
-};
-
 const input = fs
   .readFileSync(process.cwd() + "/input.txt", "utf-8")
   .trim()
@@ -13,20 +9,14 @@ const pairs = input
   .map(pair => pair.split(","))
   .map(rangePair => rangePair.flatMap(range => range.split("-")).map(Number));
 
-const p1 = pairs.reduce(
-  (total, pair) =>
-    total +
-    isOverlapping(
-      (pair[0] >= pair[2] && pair[1] <= pair[3]) ||
-        (pair[2] >= pair[0] && pair[3] <= pair[1])
-    ),
-  0
-);
+const overlapsInP1 = pair =>
+  (pair[0] >= pair[2] && pair[1] <= pair[3]) ||
+  (pair[2] >= pair[0] && pair[3] <= pair[1]);
 
-const p2 = pairs.reduce(
-  (total, pair) =>
-    total + isOverlapping(pair[0] <= pair[3] && pair[1] >= pair[2]),
-  0
-);
+const overlapsInP2 = pair => pair[0] <= pair[3] && pair[1] >= pair[2];
+
+const p1 = pairs.filter(overlapsInP1).length;
+
+const p2 = pairs.filter(overlapsInP2).length;
 
 console.log({ p1, p2 });
