e47e8eb19caedd8d4293fd8a62d14b13cecbc4bb
projectnevz@gmail.com <projectnevz@gmail.com>
2022-12-04 14:35:26 +0800

    format code

--- day4/index.js
+++ day4/index.js
@@ -1,32 +1,32 @@
 const fs = require("fs");
 
 const isOverlapping = condition => {
-    return condition ? 1 : 0;
+  return condition ? 1 : 0;
 };
 
 const input = fs
-    .readFileSync(process.cwd() + "/input.txt", "utf-8")
-    .trim()
-    .split("\n");
+  .readFileSync(process.cwd() + "/input.txt", "utf-8")
+  .trim()
+  .split("\n");
 
 const pairs = input
-    .map(pair => pair.split(","))
-    .map(rangePair => rangePair.flatMap(range => range.split("-")).map(Number));
+  .map(pair => pair.split(","))
+  .map(rangePair => rangePair.flatMap(range => range.split("-")).map(Number));
 
 const p1 = pairs.reduce(
-    (total, pair) =>
-        total +
-        isOverlapping(
-            (pair[0] >= pair[2] && pair[1] <= pair[3]) ||
-                (pair[2] >= pair[0] && pair[3] <= pair[1])
-        ),
-    0
+  (total, pair) =>
+    total +
+    isOverlapping(
+      (pair[0] >= pair[2] && pair[1] <= pair[3]) ||
+        (pair[2] >= pair[0] && pair[3] <= pair[1])
+    ),
+  0
 );
 
 const p2 = pairs.reduce(
-    (total, pair) =>
-        total + isOverlapping(pair[0] <= pair[3] && pair[1] >= pair[2]),
-    0
+  (total, pair) =>
+    total + isOverlapping(pair[0] <= pair[3] && pair[1] >= pair[2]),
+  0
 );
 
 console.log({ p1, p2 });
