2cac8b82850ec6ece7ec0d44611a22278f9b35b0
projectnevz@gmail.com <projectnevz@gmail.com>
2022-12-08 18:45:48 +0800

    day 8 solution

---
+++ day8/index.js
@@ -0,0 +1,60 @@
+const fs = require('fs')
+
+const input = fs.readFileSync('input.txt', 'utf-8').trim().split('\n')
+
+const grid = input.map(row => row.split('').map(Number))
+
+const edgeTreesCount = 4 * grid.length - 4;
+
+const countViewingDistance = (view, currTree) => {
+  let count = 0;
+
+  for (let i = 0; i < view.length; ++i) {
+    if (currTree <= view[i]) {
+      count++;
+      return count;
+    }
+
+    count++;
+  }
+
+  return count;
+}
+
+const getVisibleTrees = grid => {
+  let visibleTrees = 0;
+  let maxScenicScore = 0;
+  let viewingDistance = 1;
+
+  for (let i = 1; i < grid.length - 1; ++i) {
+    for (let j = 1; j < grid[i].length - 1; ++j) {
+      const tree = grid[i][j];
+
+      const topOfTree = grid.map(row => row[j]).slice(0, i).reverse();
+      const bottomOfTree = grid.map(row => row[j]).slice(i + 1);
+      const rightOfTree = grid[i].slice(j + 1);
+      const leftOfTree = grid[i].slice(0, j).reverse();
+
+      viewingDistance *= countViewingDistance(topOfTree, tree);
+      viewingDistance *= countViewingDistance(bottomOfTree, tree);
+      viewingDistance *= countViewingDistance(rightOfTree, tree);
+      viewingDistance *= countViewingDistance(leftOfTree, tree);
+
+      if (tree > Math.max(...rightOfTree)
+        || tree > Math.max(...leftOfTree)
+        || tree > Math.max(...topOfTree)
+        || tree > Math.max(...bottomOfTree)) {
+        visibleTrees++;
+      }
+
+      maxScenicScore = Math.max(maxScenicScore, viewingDistance);
+      viewingDistance = 1;
+    }
+  }
+
+  return { visibleTrees, maxScenicScore };
+}
+
+const { visibleTrees, maxScenicScore } = getVisibleTrees(grid)
+
+console.log({ p1: visibleTrees + edgeTreesCount, p2: maxScenicScore })
