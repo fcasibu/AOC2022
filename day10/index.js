8958b53fb9f9abcb0106cd9fa25cce52b6966b54
projectnevz@gmail.com <projectnevz@gmail.com>
2022-12-10 19:13:17 +0800

    add comment

--- day10/index.js
+++ day10/index.js
@@ -6,9 +6,34 @@ const instructions = fs.readFileSync(__dirname + "/input.txt", "utf8")
   .map(line => line.split(" "))
   .map(el => ({ op: el[0], arg: Number(el[1] ?? 0) }));
 
+/*
+  * If any of hte pixels in the sprite is on the currently drawn pixel (overlapping) 
+  * we return true so it draws a sprite or block to represent a "lit" pixel, 
+  * otherwise it's just a dot to represent a "dark" pixel
+  *
+  * In their example:
+  
+    Sprite position: ###.....................................
+
+    Start cycle   1: begin executing addx 15
+    During cycle  1: CRT draws pixel in position 0
+    Current CRT row: #
+
+  * The register is still at 1 so the sprite position is going to be placed
+  * at position 0, the middle being 1 as per the register
+  *
+  * The sprite is 3 pixels wide, therefore two of those are overlapping 
+  * pixels and with addx there's two cycles, so on those
+  * two cycles, it will draw a lit pixel in position 0 and 1 
+  *
+  * The new sprite position will now be at position 15, middle at 16 because 
+  * the register is at 16 because of addx 15
+  * so the following cycles will only draw a "dark" pixel
+  * since the sprite is not on the pixel anymore
+  */
 const isSpriteOnPixel = (cycle, register, SCREEN_WIDTH) => {
   const spritePosition = cycle % SCREEN_WIDTH;
-  return Math.abs(register - spritePosition) <= 1
+  return Math.abs(register - spritePosition) <= 1;
 }
 
 const runProgram = (instructions) => {
